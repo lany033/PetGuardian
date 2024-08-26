66f5decf5639fd42a6fd621e2e93d16a5414d91b
Melanie <mmantilla930@gmail.com>
2024-08-25 19:27:15 -0500

    fixing PetDetailScreen

--- app/src/main/java/com/lab/petguardian/ui/common/CommonBackButton.kt
+++ app/src/main/java/com/lab/petguardian/ui/common/CommonBackButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -11,18 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun CommonBackButton(onClickBackButton: () -> Unit){
-    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+fun CommonBackButton(modifier: Modifier? = null, onClickBackButton: () -> Unit){
+    Row(modifier = modifier?.fillMaxWidth() ?: Modifier, verticalAlignment = Alignment.CenterVertically) {
         IconButton(onClick = { onClickBackButton() }) {
             Icon(
-                imageVector = Icons.Filled.ArrowBack,
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                tint = Color.White,
                 contentDescription = "back",
                 modifier = Modifier.size(30.dp)
             )
         }
-        Text(text = "Back")
+        Text(text = "Back", color = Color.White, fontWeight = FontWeight.Bold, fontSize = 15.sp)
     }
 }
--- app/src/main/java/com/lab/petguardian/ui/screens/PetDetailScreen.kt
+++ app/src/main/java/com/lab/petguardian/ui/screens/PetDetailScreen.kt
@@ -1,14 +1,27 @@
 package com.lab.petguardian.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.areStatusBarsVisible
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.captionBar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsIgnoringVisibility
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -16,93 +29,133 @@ import androidx.compose.material.icons.filled.Balance
 import androidx.compose.material.icons.filled.CatchingPokemon
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Palette
+import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.lab.petguardian.R
+import com.lab.petguardian.ui.common.CommonBackButton
 import com.lab.petguardian.ui.theme.Geraldine
+import com.lab.petguardian.ui.theme.PetGuardianTheme
 
 @Composable
 fun PetDetailScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(R.mipmap.cat_love_dog),
-            contentDescription = "Foto de perfil por defecto",
+    var context = LocalContext.current
+    Scaffold(topBar = {
+        CommonBackButton(
             modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.TopCenter),
-            contentScale = ContentScale.Crop
-        )
-        Card(
+                .padding(top = WindowInsets.safeContent.asPaddingValues().calculateTopPadding()),
+            onClickBackButton = { Toast.makeText(context, "Back", Toast.LENGTH_SHORT).show() })
+    }) { it ->
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth(), shape = RoundedCornerShape(30.dp),
-            colors = CardDefaults.cardColors(containerColor = Geraldine)
+                .fillMaxSize()
+                .padding(bottom = it.calculateBottomPadding())
         ) {
-            Card(
-                modifier = Modifier.padding(horizontal = 30.dp).padding(top = 20.dp),
-                shape = RectangleShape,
-                colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-            ) {
-                Text(text = "Michina", fontWeight = FontWeight.Bold, fontSize = 25.sp)
-                Text(text = "2 years old")
-                Spacer(modifier = Modifier.height(20.dp))
-                Row(modifier = Modifier.padding(bottom = 20.dp)) {
-                    Card(shape = RectangleShape) {
-                        Icon(
-                            modifier = Modifier.size(30.dp),
-                            imageVector = Icons.Default.CatchingPokemon,
-                            contentDescription = "pet"
+            Image(
+                painter = painterResource(R.mipmap.cat_love_dog),
+                contentDescription = "Foto de perfil por defecto",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black.copy(alpha = 0.3f)),
+                            startY = 500f,
+                            endY = 0f
                         )
-                        Text(text = "Sex")
-                        Text(text = "Male", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-                    }
-                    Card {
-                        Icon(imageVector = Icons.Default.Palette, contentDescription = "pet")
-                        Text(text = "Sex")
-                        Text(text = "Male")
-                    }
-                    Card {
-                        Icon(imageVector = Icons.Default.Check, contentDescription = "pet")
-                        Text(text = "Sex")
-                        Text(text = "Male")
-                    }
-                    Card {
-                        Icon(imageVector = Icons.Default.Balance, contentDescription = "pet")
-                        Text(text = "Sex")
-                        Text(text = "Male")
-                    }
-                }
-            }
-
+                    )
+            )
             Card(
                 modifier = Modifier
-                    .height(250.dp)
+                    .align(Alignment.BottomCenter)
+                    .height(50.dp).fillMaxWidth(),
+                shape = RectangleShape
+            ) {}
+            Card(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .zIndex(1f), shape = RoundedCornerShape(30.dp),
+                colors = CardDefaults.cardColors(containerColor = Geraldine)
             ) {
-                Text(
-                    modifier = Modifier.padding(horizontal = 20.dp).padding(top = 20.dp),
-                    text = "Next Plans",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
-                )
-                LazyColumn(modifier = Modifier.padding(20.dp)) {
-                    items(6) {
-                        PlanItem()
+                Card(
+                    modifier = Modifier
+                        .padding(horizontal = 30.dp)
+                        .padding(top = 20.dp),
+                    shape = RectangleShape,
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+                ) {
+                    Text(text = "Michina", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+                    Text(text = "2 years old")
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Row(
+                        modifier = Modifier
+                            .padding(bottom = 20.dp)
+                            .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        PetFeatureItem(
+                            featureName = "Type",
+                            featureValue = "Cat",
+                            icon = Icons.Default.CatchingPokemon
+                        )
+
+                        PetFeatureItem(
+                            featureName = "Sex",
+                            featureValue = "Male",
+                            icon = Icons.Default.Pets
+                        )
+                        PetFeatureItem(
+                            featureName = "Color",
+                            featureValue = "Naranjoso",
+                            icon = Icons.Default.Palette
+                        )
+                        PetFeatureItem(
+                            featureName = "Weight",
+                            featureValue = "5 kg",
+                            icon = Icons.Default.Balance
+                        )
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .height(250.dp)
+                        .zIndex(1f),
+                    shape = RoundedCornerShape(30.dp)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .padding(top = 20.dp),
+                        text = "Next Plans",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                    LazyColumn(modifier = Modifier.padding(10.dp)) {
+                        items(6) {
+                            PlanItem()
+                        }
                     }
                 }
             }
@@ -110,8 +163,27 @@ fun PetDetailScreen() {
     }
 }
 
-@Preview
+@Composable
+fun PetFeatureItem(featureName: String, featureValue: String, icon: ImageVector) {
+    Column(
+        modifier = Modifier.padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(30.dp),
+            imageVector = icon,
+            contentDescription = "pet"
+        )
+        Text(text = featureName)
+        Text(text = featureValue, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+    }
+}
+
+
+@Preview(showSystemUi = true)
 @Composable
 fun PetDetailScreenPreview() {
-    PetDetailScreen()
+    PetGuardianTheme {
+        PetDetailScreen()
+    }
 }
